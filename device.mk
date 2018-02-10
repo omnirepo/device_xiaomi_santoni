1b4776b0a8a87806bfca3cf62885b804c91901b5
Nikesh001 <nikkataria74@gmail.com>
2018-02-10 07:04:43 +0000

    santoni: Cleanup display packages

--- device.mk
+++ device.mk
@@ -161,11 +161,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
-    liboverlay \
-    libgenlock \
     libdisplayconfig \
-    libjni_livedisplay \
-    libtinyxml
+    liboverlay
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -174,9 +171,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
